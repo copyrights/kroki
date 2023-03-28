bb0953755b5451a8eb790e2da934cd620b96ea06
copyrights <copyrights@mailbox.org>
2023-03-28 22:41:20 +0200

    add sudo

--- upgrade-buildah.sh
+++ upgrade-buildah.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
-apt-get -y -qq update
-apt-get -y install software-properties-common
-add-apt-repository -y ppa:alexlarsson/flatpak
-add-apt-repository -y ppa:gophers/archive
-apt-add-repository -y ppa:projectatomic/ppa
-apt-get -y -qq update
-apt-get -y install bats btrfs-tools git libapparmor-dev libdevmapper-dev libglib2.0-dev libgpgme11-dev libseccomp-dev libselinux1-dev skopeo-containers go-md2man
-apt-get -y install golang-1.13
+sudo apt-get -y -qq update
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository -y ppa:alexlarsson/flatpak
+sudo add-apt-repository -y ppa:gophers/archive
+sudo apt-add-repository -y ppa:projectatomic/ppa
+sudo apt-get -y -qq update
+sudo apt-get -y install bats btrfs-tools git libapparmor-dev libdevmapper-dev libglib2.0-dev libgpgme11-dev libseccomp-dev libselinux1-dev skopeo-containers go-md2man
+sudo apt-get -y install golang-1.13
 
 mkdir ~/buildah
 cd ~/buildah
@@ -15,5 +15,5 @@ export GOPATH=`pwd`
 git clone https://github.com/containers/buildah ./src/github.com/containers/buildah
 cd ./src/github.com/containers/buildah
 PATH=/usr/lib/go-1.13/bin:$PATH make runc all SECURITYTAGS="apparmor seccomp"
-make install install.runc
+sudo make install install.runc
 buildah --help
